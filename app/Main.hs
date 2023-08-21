4db70fddb6241a0d48b337f1c90502eb0c0b0742
xieyuschen <xieyuschen@gmail.com>
2023-08-21 19:26:15 +0800

    feature: read and print request data

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,32 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+-- option+shift+f to format
+import Auth (getGithubKey)
+import Control.Monad.Trans.Except (runExceptT)
+import Data (Author (date), Commit (author), CommitDetail (commit))
+import Data.Time (getCurrentTime)
+import GHC.Stack (getCurrentCCS)
+import Github (sendRequest, withinOneDay)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = app
+
+app :: IO ()
+app = do
+   keyEx <- runExceptT getGithubKey
+   let Right key = keyEx
+   putStrLn key
+   result <- sendRequest "hehonghui/awesome-english-ebooks" key
+   case result of
+      Nothing -> print "fail to get response data"
+      Just info -> do
+         now <- getCurrentTime
+         if withinOneDay now $ date $ author $ commit info
+            then print info
+            else print "no recently(1 day) updated books"
---
+++ lib/github/Data.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+module Data where
+
+import Data.Aeson (
+    FromJSON (parseJSON),
+    withObject,
+    (.:),
+    (.:?),
+ )
+import Data.Text (Text)
+import Data.Time (UTCTime)
+
+data CommitDetail = CommitDetail
+    { sha :: Text
+    , commit :: Commit
+    , files :: [File]
+    }
+    deriving (Show)
+
+instance FromJSON CommitDetail where
+    parseJSON = withObject "CommitDetail" $ \o ->
+        CommitDetail
+            <$> o
+            .: "sha"
+            <*> o
+            .: "commit"
+            <*> o
+            .: "files"
+
+newtype Commit = Commit
+    { author :: Author
+    }
+    deriving (Show)
+
+instance FromJSON Commit where
+    parseJSON = withObject "Commit" $ \o ->
+        Commit
+            <$> o
+            .: "author"
+
+data Author = Author
+    { name :: Text
+    , date :: UTCTime
+    }
+    deriving (Show)
+
+instance FromJSON Author where
+    parseJSON = withObject "Author" $ \o ->
+        Author
+            <$> o
+            .: "name"
+            <*> o
+            .: "date"
+
+data File = File
+    { filename :: Text
+    , status :: Text
+    , raw_url :: Text
+    }
+    deriving (Show)
+
+instance FromJSON File where
+    parseJSON = withObject "File" $ \o ->
+        File
+            <$> o
+            .: "filename"
+            <*> o
+            .: "status"
+            <*> o
+            .: "raw_url"
