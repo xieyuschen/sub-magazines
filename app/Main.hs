4abbb1e6e38440decfec5050c64889c818152e72
xieyuschen <xieyuschen@gmail.com>
2023-08-22 19:16:20 +0800

    refactor: rearrange packages

--- app/Main.hs
+++ app/Main.hs
@@ -19,13 +19,14 @@ main = app
 
 app :: IO ()
 app = do
-   keyEx <- runExceptT getGithubKey
-   let Right key = keyEx
-   result <- sendRequest "hehonghui/awesome-english-ebooks" key
-   case result of
-      Nothing -> print "fail to get response data"
-      Just info -> do
-         now <- getCurrentTime
-         if withinOneDay now $ date $ author $ commit info
-            then print info
-            else print "no recently(1 day) updated books"
+    keyEx <- runExceptT getGithubKey
+    let Right key = keyEx
+    putStrLn key
+    result <- sendRequest "hehonghui/awesome-english-ebooks" key
+    case result of
+        Nothing -> print "fail to get response data"
+        Just info -> do
+            now <- getCurrentTime
+            if withinOneDay now $ date $ author $ commit info
+                then print info
+                else print "no recently(1 day) updated books"
