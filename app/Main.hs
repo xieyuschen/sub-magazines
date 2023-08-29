ce8248d3bc69a11aed5978b420fa09dc36c21b78
xieyuschen <xieyuschen@gmail.com>
2023-08-29 19:12:04 +0800

    refine: improve logs

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,7 @@ import Data (Author (date), Commit (author), CommitDetail (commit, files), Email
 import Data.Aeson (decode)
 import Data.ByteString.Lazy.UTF8 as BLU (fromString)
 import qualified Data.Text as Text
-import Data.Time (getCurrentTime)
+import Data.Time (defaultTimeLocale, formatTime, getCurrentTime)
 import Email (sendEmailOut)
 import GHC.Stack (getCurrentCCS)
 import Github (sendRequest, withinOneDay)
@@ -27,22 +27,27 @@ main = app
 app :: IO ()
 app = do
     let logger = logStringStdout
+    logger <& "start cronjob of sub-magazine"
     keyEx <- runExceptT getGithubKey
     let Right key = keyEx
     rawConfEx <- runExceptT getEmailConfig
 
     let Right rawConf = rawConfEx
-
+    logger <& "prepare to send request"
     result <- sendRequest "hehonghui/awesome-english-ebooks" key
-
+    logger <& "get response from hehonghui/awesome-english-ebooks"
     case result of
         Nothing -> logger <& "fail to get response data"
         Just info -> do
             now <- getCurrentTime
-            if withinOneDay now $ date $ author $ commit info
+            logger <& "print the time:"
+            logger <& ("now is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" now)
+            let lastUpdate = date $ author $ commit info
+            logger <& ("last time date is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" lastUpdate)
+            if withinOneDay now lastUpdate
                 then do
                     case decode $ BLU.fromString rawConf of
-                        Nothing -> logger <& "fail to analyze json to Email Config"
+                        Nothing -> logger <& "fail to analyze json to email config"
                         Just emailConf -> do
                             let filters = liftA2 (&&) (\x -> status x == Text.pack "added") (\x -> Text.isSuffixOf (raw_url x) $ Text.pack ".epub")
                             let addedUrl = map raw_url $ filter filters (files info)
