1ff59fc8815725be65acd3ddbdc803c0cd1c2362
Griffin <xieyuschen@gmail.com>
2023-08-31 17:51:59 +0800

    refine: log with severity and code line (#8)

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Auth (getEmailConfig, getGithubKey)
-import Colog.Core (LogAction (..), logStringStdout, (<&))
 import Control.Applicative (Applicative (liftA2))
 import Control.Monad (join)
-import Control.Monad.Cont (forM_)
+import Control.Monad.Cont (MonadTrans (lift), forM_)
 import Control.Monad.Trans.Except (runExceptT)
 import Data (Author (date), Commit (author), CommitDetail (commit, files), EmailConfig, File (raw_url, status))
 import Data.Aeson (decode)
@@ -21,35 +23,69 @@ import Email (sendEmailOut)
 import GHC.Stack (getCurrentCCS)
 import Github (sendRequest, withinOneDay)
 
+import Colog (
+    HasLog (..),
+    LogAction,
+    Message,
+    SimpleMsg,
+    WithLog,
+    cmap,
+    fmtMessage,
+    fmtSimpleMessage,
+    formatWith,
+    log,
+    logDebug,
+    logError,
+    logInfo,
+    logStringStdout,
+    logText,
+    logTextStderr,
+    logTextStdout,
+    richMessageAction,
+    usingLoggerT,
+    (<&),
+    pattern D,
+    pattern E,
+    pattern I,
+ )
+import Control.Monad.Reader (ReaderT (ReaderT, runReaderT))
+import GHC.Base (IO (IO))
+import Prelude hiding (log)
+
 main :: IO ()
 main = app
 
+logStdoutAction :: LogAction IO Message
+logStdoutAction = cmap fmtMessage logTextStdout
+
+runLog s str = usingLoggerT logStdoutAction $ log s str
+
 app :: IO ()
 app = do
-    let logger = logStringStdout
-    logger <& "start cronjob of sub-magazine"
+    runLog I "start cronjob of sub-magazine"
+    runLog I "read credentials from environment"
     keyEx <- runExceptT getGithubKey
     let Right key = keyEx
     rawConfEx <- runExceptT getEmailConfig
 
     let Right rawConf = rawConfEx
-    logger <& "prepare to send request"
+    runLog I "prepare to get newest commit of repo hehonghui/awesome-english-ebook from github"
     result <- sendRequest "hehonghui/awesome-english-ebooks" key
-    logger <& "get response from hehonghui/awesome-english-ebooks"
     case result of
-        Nothing -> logger <& "fail to get response data"
+        Nothing -> runLog E "fail to get response data"
         Just info -> do
+            runLog I "get response from github successfully"
             now <- getCurrentTime
-            logger <& "print the time:"
-            logger <& ("now is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" now)
+            runLog I $ Text.pack ("current time is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" now)
             let lastUpdate = date $ author $ commit info
-            logger <& ("last time date is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" lastUpdate)
+            runLog I $ Text.pack ("last time date is: " ++ formatTime defaultTimeLocale "%m/%d/%Y %I:%M %p" lastUpdate)
             if withinOneDay now lastUpdate
                 then do
                     case decode $ BLU.fromString rawConf of
-                        Nothing -> logger <& "fail to analyze json to email config"
+                        Nothing -> runLog E "fail to analyze json to email config"
                         Just emailConf -> do
                             let filters = liftA2 (&&) (\x -> status x == Text.pack "added") (\x -> Text.isSuffixOf (raw_url x) $ Text.pack ".epub")
                             let addedUrl = map raw_url $ filter filters (files info)
+                            runLog D $ Text.unlines addedUrl
                             forM_ addedUrl (sendEmailOut emailConf)
-                else logger <& "no recently(1 day) updated books"
+                else runLog I "no recently(1 day) updated books"
